734358871a408db95869cea12f031be86233fd7b
[SashaPLS] <[playastaaa@mail.ru]>
2022-08-11 21:16:55 +0300

    last

--- Kameneckiy_csharp3/Seminar_Array_Function/seminar_3/Program.cs
+++ Kameneckiy_csharp3/Seminar_Array_Function/seminar_3/Program.cs
@@ -184,3 +184,26 @@ void SqrRange(int a)
 int a = ReadInt("Введите число: ");
 SqrRange(a);
 
+//////
+
+int number = ReadInt("Введите число N: ");
+int i = 1;
+
+while(i <= number)
+{
+    Console.Write(Sqr(i) + " ");
+    i++;
+}
+
+int Sqr(int a)
+{
+    return a * a;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
