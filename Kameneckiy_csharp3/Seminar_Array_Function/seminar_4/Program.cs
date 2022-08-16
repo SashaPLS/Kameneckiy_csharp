7dbb386341e9b182df13ed7f65c4a837c0f933ec
[SashaPLS] <[playastaaa@mail.ru]>
2022-08-16 21:57:56 +0300

    seminar_4

---
+++ Kameneckiy_csharp3/Seminar_Array_Function/seminar_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+int sum = 0;
+
+while (index <= number)
+{
+sum += index++;
+}
+Console.WriteLine(sum);
+
+/* for ( int i =0 ; i <= index ; i++)
+    {
+        sum +=i ;
+    }
+*/
